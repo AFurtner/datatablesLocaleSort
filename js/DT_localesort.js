e101da8f969a7923d0ed92a5a6c6d0a839f9df42
unknown <furtner@DE-ADN-BVBFKV1.adesso.local>
2015-07-20 14:44:50 +0200

    - make plugin work with multiple tables - supply case insensitive initialisation option - update documentation

--- js/DT_localesort.js
+++ js/DT_localesort.js
@@ -8,11 +8,11 @@
  * e.g in German correct sort order is: Arzt, Ärzte, Ast, Baum, Zeder
  * in contrast to Arzt, Ast, Baum, Zeder, Ärzte as in English/ASCII string sort
  *
- * Since String.localeCompare is extremely expansive performance wise this uses the following approach:
- * Pre sort all column fields using String.localeCompare (onyl when necessary) and then map each cell to the position in the ordered list and cache that.
- * Later DataTables.api().sort() just uses the position index (an integer).
- * The sorting can then take advantage of much faster integer comparison (even faster than ASCII string comparison).
+ * Since String.localeCompare has extremely bad performance this uses the following approach:
+ * Sort ONCE the assigend columns using String.localeCompare (or String compare when sufficient), then cache a map of each cell to the position in the ordered list.
+ * Later DataTables.api().sort() just uses the position index and can then take advantage of much faster integer comparison.
  * For large data sets this can be more than 100 times faster than the naive localeCompare approach.
+ * This should even speed up sorting when there is only ASCII data.
  *
  * Please note that the cache must be invalidated and rebuild whenever the underlying
  * data changes (e.g. Rows added/removed). The plugin does not check this, user has to invalidate here.
@@ -20,124 +20,171 @@
  *
  *
  * usage:
- * drop in DT_localsort.js, include script in your html.
+ * drop in DT_localesort.js, include script in your html.
  * In initialisation options set `"type": "string-locale-mapped-int"` and `"orderDataType":"string-locale-mapped-int"`
  * for each column that shall benefit from  sorted with this plugin.
  *
- *  @name Locale (string) mapped int
- *  @summary Sort respecting Locale, caches order, uses integer compare for sorting
- *  @author [Andreas Furtner](https://github.com/AFurtner/)
-
- *  @example
+ * @summary Sort respecting Locale, caches order, uses integer compare for sorting
+ *
+ * @author [Andreas Furtner](https://github.com/AFurtner/)
+ *
+ * @example
+ * ´´´
  * $('#example').dataTable( {
  *				 "columnDefs": [ {
  *								 'searchable':true,
  *								 "type": "string-locale-mapped-int",
  *								 "orderDataType":"string-locale-mapped-int",
  *								 "targets": [ 0, 1 ]
- *				 } ]
+ *				 } ],
+ *				 "stringLocaleMapped": { "caseInsensitive": false }, // optional, default is to order case insensitive, set to false if sort shall respect case
  * });
- * @author Andreas Furtner
+ *
+ * // after adding/ modifing rows which must be reflected in a different sort order:
+ * $('#example').api().invalidateStringLocaleSortCache() // will recalc caches on next sort
+ * // or
+ * $('#example').api().recalcStringLocaleSortCache() // will recalc caches immediately
+ *
+ * ´´´
  */
 (function(){
-	var factory = function( $, DataTable ) {
-		"use strict";
+  var factory = function( $, DataTable ) {
+    "use strict";
+
+    // invalidate lookup maps for all registered columns, will recalculate caches on next sort (normally executed on next draw())
+    $.fn.dataTable.Api.register( 'invalidateStringLocaleSortCache()', function () {
+      this.iterator( 'table', function ( context ) {
+        context.stringLocaleMapped.cache = [];
+      } );
+
+      return this;
+    } );
+
+    // invalidate lookup maps for all registered columns
+    $.fn.dataTable.Api.register( 'recalcStringLocaleSortCache()', function () {
+      this.iterator( 'table', function ( context ) {
+        context.stringLocaleMapped.cache = [];
+        context.aoColumns.forEach( function(col, colIdx) {
+          if (col.bSortable && col.sSortDataType == "string-locale-mapped-int") {
+            buildStringLocaleMappedIntColumn(context, colIdx, context.oInstance.api().column(colIdx, {order: 'index'}).data())
+          }
+        });
+      } );
+
+      return this;
+    } );
 
 
-		var columnsSortOrderMap = [];
-		DataTable.ext.oApi.fnInvalidateStringLocaleSortCache = function () {
-				columnsSortOrderMap = [];
-		};
+    function init(settings) {
+      // this serves as a container for our internal stuff
+      settings.stringLocaleMapped = {};
 
-		/*
-		 * fast sorting relies on this sorter which maps
-		 * locale aware sort to sort positions once!
-		 * So the expensive locale compare is done at most once!
-		 * afterwards DT just uses much faster integer comparison
-		 */
-		function buildStringLocaleMappedIntColumn(col, colData){
-				function onlyAscii(elem) {
-						for (var j = 0; j < elem.length; ++j) {
-								if ((0xFF80 & elem.charCodeAt(j)) != 0) {
-										return false;
-								}
-						}
-						return true;
-				}
+      var myOptions = settings.oInit.stringLocaleMapped;
+      // always set this option
+      settings.stringLocaleMapped.caseInsensitive = myOptions && myOptions.caseInsensitive === false ? false : true;
+      settings.stringLocaleMapped.cache = [];
+    }
 
-				var tmpMap = colData.map(function (elem, i) {
-						var asciiOnly = onlyAscii(elem);
-						return {"index":i, "data": asciiOnly ?  elem.toLowerCase() : elem.toLocaleLowerCase(), "isAscii" : asciiOnly}
-				});
+    function getSortColumnData(tableInstance, settings, colIdx) {
+      if (! settings.stringLocaleMapped) {
+        init(settings);
+      }
+      if (! settings.stringLocaleMapped.cache[colIdx] ) {
+
+        return buildStringLocaleMappedIntColumn(settings, colIdx, tableInstance.api().column(colIdx, {order: 'index'}).data());
+      }
+      return settings.stringLocaleMapped.cache[colIdx];
+    }
+
+    /*
+     * fast sorting relies on this sorter which maps
+     * column cell indexes to (locale respecting) sort positions once!
+     * So the expensive localeCompare is not done on each dataTable._fnSort()!
+     * That just uses much faster integer comparison afterwards, as long as caches are not invalidated
+     */
+    function buildStringLocaleMappedIntColumn(context, col, colData){
+      function onlyAscii(elem) {
+        for (var j = 0; j < elem.length; ++j) {
+          if ((0xFF80 & elem.charCodeAt(j)) != 0) {
+            return false;
+          }
+        }
+        return true;
+      }
 
 
-				tmpMap.sort(function (x, y) {
-						return (x.isAscii && y.isAscii)
-								? (x.data > y.data ? 1 : -1)
-								: x.data.localeCompare(y.data); // EXPENSIVE compare, we want to do this only once per column, and not in the Array.sort() loops (O(n*log(n)) average case for merge/quick)
-				});
-				columnsSortOrderMap[col]= new Array(tmpMap.length);
+      var tmpMap = colData.map(function (elem, i) {
+        var asciiOnly = onlyAscii(elem);
+        return {
+          "index":i,
+          "data": context.stringLocaleMapped.caseInsensitive ? (asciiOnly ?  elem.toLowerCase() : elem.toLocaleLowerCase()) : elem,
+          "isAscii" : asciiOnly
+        }
+      });
 
-				columnsSortOrderMap[col][ tmpMap[0].index ] = 0; // factored out 1st entry to take array index i-1 check out of the loop
-				var idxForSameContent = 0;
 
-				for (var i = 1; i< tmpMap.length; i++) {
-						if (tmpMap[i].data == tmpMap[i-1].data) {
-								// if elems equal, keep sort index equal to make multi column sort possible later
-								columnsSortOrderMap[col][ tmpMap[i].index ] = idxForSameContent;
-						} else {
-								// default
-								columnsSortOrderMap[col][ tmpMap[i].index ] = i;
-								idxForSameContent = i;
-						}
-				}
+      // EXPENSIVE sort, we want to do this only once per column, and not
+      // in per DataTable.sort() (which internally uses Array.sort(): O(n*log(n)) for merge/quick sort)
+      tmpMap.sort(function (x, y) {
+        return (x.isAscii && y.isAscii)
+            ? (x.data > y.data ? 1 : -1)
+            : x.data.localeCompare(y.data);
+      });
+      context.stringLocaleMapped.cache[col]= new Array(tmpMap.length);
+      var colToOrderPos = context.stringLocaleMapped.cache[col];
 
-				return columnsSortOrderMap[col];
-		}
+      colToOrderPos[ tmpMap[0].index ] = 0; // factored out 1st entry to take array index i-1 check out of the loop
+      var idxForSameContent = 0;
 
-		$.extend( DataTable.ext.order, {
-				"string-locale-mapped-int": function (settings, col) {
-						if (! columnsSortOrderMap[col] ) {
+      for (var i = 1; i< tmpMap.length; i++) {
+        if (tmpMap[i].data == tmpMap[i-1].data) {
+          // if elems equal, keep sort index equal to make multi column sort possible later
+          colToOrderPos[ tmpMap[i].index ] = idxForSameContent;
+        } else {
+          // default
+          colToOrderPos[ tmpMap[i].index ] = i;
+          idxForSameContent = i;
+        }
+      }
 
-								return buildStringLocaleMappedIntColumn(col, this.api().column(col, {order: 'index'}).data());
-						}
-						return columnsSortOrderMap[col];
-				}
-		});
-		$.extend( DataTable.ext.type.order, {
+      return colToOrderPos;
+    }
 
-				/*
-				 * we need this, so that DataTable does not try to use one of the following locale compares
-				 * for each sorting but instead uses integer comparison on the pre-computed numbers from above
-				 */
-				"string-locale-mapped-int-pre": function (a) { return a; }  //,
+    $.extend( DataTable.ext.order, {
+      "string-locale-mapped-int": function (settings, colIdx) {
+        return getSortColumnData(this, settings, colIdx);
+      }
+    });
 
-				/*
-				 * this is what we used before to get the desired behaviour: the two functions were called for every compare in the inner sort loop!
-				 * --> really slow!
-				 * following two are not needed/ called by DT any more,
-				 * since the sorting relies on the above sort cache which maps
-				 * locale aware sort to sort positions once!
-				 * So the expensive locale compare (and not so much but still expensive String.compare) is done only once!
-				 * afterwards DT just uses much faster integer comparison
-				 */
-				//"string-locale-asc": function (x, y) { return x.localeCompare(y); },
-				//"string-locale-desc": function (x, y) { return y.localeCompare(x); }
-		});
+    $.extend( DataTable.ext.type.order, {
 
-	}; // /factory
+      /*
+       * we need this 'formatter', so that DataTable does not try to use the fallback string-asc|desc compare functions one of the following locale compares
+       * for each sorting but instead uses integer comparison on the pre-computed numbers from above
+       */
+      "string-locale-mapped-int-pre": function (a) { return a; }
+      //,
+      /*
+       * this is would be the naive approach for locale sorting: the two functions would be called for every compare in the inner sort loop in dataTable._fnSort()!
+       * --> really slow!
+       */
+      //"string-locale-asc": function (x, y) { return x.localeCompare(y); },
+      //"string-locale-desc": function (x, y) { return y.localeCompare(x); }
+    });
+
+  }; // /factory
 
 // Define as an AMD module if possible
-	if ( typeof define === 'function' && define.amd ) {
-		define( ['jquery', 'datatables'], factory );
-	}
-	else if ( typeof exports === 'object' ) {
-		// Node/CommonJS
-		factory( require('jquery'), require('datatables') );
-	}
-	else if ( jQuery ) {
-		// Otherwise simply initialise as normal, stopping multiple evaluation
-		factory( jQuery, jQuery.fn.dataTable );
-	}
+  if ( typeof define === 'function' && define.amd ) {
+    define( ['jquery', 'datatables'], factory );
+  }
+  else if ( typeof exports === 'object' ) {
+    // Node/CommonJS
+    factory( require('jquery'), require('datatables') );
+  }
+  else if ( jQuery ) {
+    // Otherwise simply initialise as normal, stopping multiple evaluation
+    factory( jQuery, jQuery.fn.dataTable );
+  }
 
 })();
